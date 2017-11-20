c570b7edc6d229cf18511553d4e84d63e87fdec4
Henrique Teixeira <henriquetcro@gmail.com>
2017-11-20 15:26:36 -0200

    A User May Responde to Thread [The bug has been fixed]

--- tests/Feature/ParticipateInThreadsTest.php
+++ tests/Feature/ParticipateInThreadsTest.php
@@ -8,14 +8,14 @@ class ParticipateInThreadsTest extends TestCase
 {
     use DatabaseMigrations;
 
-//    /** @test */
-//    function unauthenticated_users_may_not_add_replies()
-//    {
-//        $this->expectException('Illuminate\Auth\AuthenticationException');
-//        $thread = factory('App\Thread')->create();
-//        $reply = factory('App\Reply')->make();
-//        $this->post($thread->path() . '/replies', $reply->toArray());
-//    }
+    /** @test */
+    function unauthenticated_users_may_not_add_replies()
+    {
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+        $thread = factory('App\Thread')->create();
+        $reply = factory('App\Reply')->make();
+        $this->post($thread->path() . '/replies', $reply->toArray());
+    }
 
     /** @test */
     function an_authenticated_user_may_participate_in_forum_threads()
