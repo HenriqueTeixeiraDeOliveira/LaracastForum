e8bcb44c2f1f877377ba6cbaea0c2660ee786707
Henrique Teixeira <henriquetcro@gmail.com>
2017-11-30 16:50:44 -0200

    Meta Details and Pagination

--- app/Thread.php
+++ app/Thread.php
@@ -13,6 +13,17 @@ class Thread extends Model
      * @var array
      */
     protected $guarded = [];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope('replyCount', function ($builder){
+            $builder->withCount('replies');
+        });
+    }
+
+
     /**
      * Get a string path for the thread.
      *
