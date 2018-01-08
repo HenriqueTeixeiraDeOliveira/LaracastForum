fb9157c5630bb8e59a1bb679923e65032e093a6a
Henrique Teixeira <henriquetcro@gmail.com>
2018-01-08 11:30:06 -0200

    A Authorized Users Can Delete Replies

--- app/Http/Controllers/RepliesController.php
+++ app/Http/Controllers/RepliesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Reply;
 use App\Thread;
 use Illuminate\Http\Request;
 
@@ -13,7 +14,7 @@ class RepliesController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth')->only('store');
+        $this->middleware('auth');
     }
 
     /**
@@ -32,4 +33,14 @@ class RepliesController extends Controller
 
         return back()->with('flash', 'Your reply has been left.');
     }
+
+    public function destroy(Reply $reply)
+    {
+        $this->authorize('update',$reply);
+
+        $reply->delete();
+
+        return back();
+    }
+
 }
