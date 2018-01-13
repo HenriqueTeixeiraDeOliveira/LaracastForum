4b170ef2d024623e80b45fb30d992eb3deb44640
Henrique Teixeira <henriquetcro@gmail.com>
2018-01-13 14:25:45 -0200

    A New Reply Component

--- app/Thread.php
+++ app/Thread.php
@@ -78,7 +78,7 @@ class Thread extends Model
      */
     public function addReply($reply)
     {
-        $this->replies()->create($reply);
+        return $this->replies()->create($reply);
     }
     /**
      * Apply all relevant thread filters.
