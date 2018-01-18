f1bf0cb1702c50f8bb10e7492102e6979413fbd4
Henrique Teixeira <henriquetcro@gmail.com>
2018-01-17 23:43:09 -0200

    Laravel and Vue Pagination

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -71,10 +71,7 @@ class ThreadsController extends Controller
      */
     public function show($channelId, Thread $thread)
     {
-        return view('threads.show', [
-            'thread' => $thread,
-            'replies' => $thread->replies()->paginate(5)
-        ]);
+        return view('threads.show', compact('thread'));
     }
 
     public function destroy($channel, Thread $thread)
--- resources/assets/js/components/NewReply.vue
+++ resources/assets/js/components/NewReply.vue
@@ -26,13 +26,8 @@
 <script>
     export default {
 
-        props: ['endpoint'],
-
         data() {
-            return {
-                body: '',
-                endpoint: ''
-            };
+            return { body: '' };
         },
 
         computed: {
@@ -43,7 +38,7 @@
 
         methods: {
             addReply() {
-                axios.post(this.endpoint, { body: this.body })
+                axios.post(location.pathname + '/replies', { body: this.body })
                     .then(({data}) => {
                     this.body = '';
                     flash('Your reply has been posted.');
